ff84872314d770c559e753787132658b3a7183a6
Matt2e2 <43849068+Matt2e2@users.noreply.github.com>
2023-02-24 19:45:01 +0200

    Removed useless selector, added box shadow

--- src/routes/order/QueueStatus.svelte
+++ src/routes/order/QueueStatus.svelte
@@ -2,31 +2,32 @@
   export let style: 'open' | 'closed' | 'selective' = 'open';
 </script>
 
-<div class="queue-status {style}">
+<div class={"order-status " + style}>
   <p class="normal-text">order status:</p>
   <p class="bold-text">{style}</p>
 </div>
 
 <style>
-  .queue-status {
+  .order-status {
     display: inline-flex;
     justify-content: center;
     align-items: center;
     border-radius: 200px;
+    color: var(--secondary-two);
     outline: solid 4px var(--accent-two);
     outline-offset: 4px;
     background-color: var(--accent-two);
     padding: 0.5rem 2rem;
     gap: 1rem;
+    box-shadow: 0 2px 130px 0 var(--accent-two-16);
   }
-  p {
-    color: var(--secondary-two)
-  }
-  .closed {
+  
+  .order-status.closed {
     background-color: var(--accent);
-    outline: solid 4px var(--accent)
+    outline: solid 4px var(--accent);
   }
-  .selective {
+
+  .order-status.selective {
     background-color: var(--secondary);
     outline: solid 4px var(--secondary);
   }
