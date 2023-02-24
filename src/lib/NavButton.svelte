09ae8ade583e046e0d60fc82112b0fcb5f1b040b
Matt2e2 <43849068+Matt2e2@users.noreply.github.com>
2023-02-24 15:01:30 +0200

    Change Nav Button style prop typing

--- src/lib/NavButton.svelte
+++ src/lib/NavButton.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  export let text: string = "button text";
-  export let style: string = "";
-  export let link: string = "/";
+  export let text: string = 'button text';
+  export let style: '' | 'active' = '';
+  export let link: string = '/';
 </script>
 
 <a class={"nav-button " + style} href={link}>
