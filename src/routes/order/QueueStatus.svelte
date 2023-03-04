5e141e34b78d3451306325617c36fe74b0e1505d
Matt2e2 <43849068+Matt2e2@users.noreply.github.com>
2023-03-04 17:36:11 +0200

    Add dashed outline to queue status

--- src/routes/order/QueueStatus.svelte
+++ src/routes/order/QueueStatus.svelte
@@ -1,34 +1,80 @@
 <script lang="ts">
+	import { onMount } from "svelte";
+
   export let style: 'open' | 'closed' | 'selective' = 'open';
+
+  let divWidth: number;
+  let divHeight: number;
+
+  $: borderWidth = divWidth + 12;
+  $: borderHeight = divHeight + 12;
+
+  $: spinSpeed = 0.5;
+  $: spinOffset = 0;
+
+  onMount(() => {
+    let frame = requestAnimationFrame(loop);
+
+    function loop() {
+      frame = requestAnimationFrame(loop);
+
+      spinOffset = spinOffset - spinSpeed;
+    }
+
+    return (()=> {
+      cancelAnimationFrame(frame);
+    })
+  })
 </script>
 
-<div class={"order-status " + style}>
-  <p class="normal-text">order status:</p>
-  <p class="bold-text">{style}</p>
+<div class="order-status-container">
+  <div class={"order-status " + style} bind:clientWidth={divWidth} bind:clientHeight={divHeight}>
+    <p class="normal-text">order status:</p>
+    <p class="bold-text">{style}</p>
+  </div>
+  <svg width={borderWidth + 4} height={borderHeight + 4} viewBox="0 0 {borderWidth + 4} {borderHeight + 4}" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="2" y="2" width={borderWidth} height={borderHeight} rx="26" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-dasharray="56 10 36 10 8 10 34 10" stroke-dashoffset={spinOffset}/>
+  </svg>
 </div>
 
 <style>
+  .order-status-container {
+    position: relative;
+  }
+
   .order-status {
     display: inline-flex;
     justify-content: center;
     align-items: center;
     border-radius: 200px;
     color: var(--secondary-two);
-    outline: solid 4px var(--accent-two);
-    outline-offset: 4px;
     background-color: var(--accent-two);
     padding: 0.5rem 2rem;
     gap: 1rem;
     box-shadow: 0 2px 130px 0 var(--accent-two-16);
   }
   
+  svg {
+    color: var(--accent-two);
+    position: absolute;
+    left: -.5rem;
+    top: -.5rem;
+    z-index: -5;
+  }
+
   .order-status.closed {
     background-color: var(--accent);
-    outline: solid 4px var(--accent);
+  }
+
+  .order-status.closed + svg {
+    color: var(-accent);
   }
 
   .order-status.selective {
     background-color: var(--secondary);
-    outline: solid 4px var(--secondary);
+  }
+
+  .order-status.selective + svg {
+    color: var(--secondary);
   }
 </style>
