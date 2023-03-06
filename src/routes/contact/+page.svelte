c9324c30ed7ee5701ff04a6db7010afd4aa6533b
trev-git <htaedshararam@gmail.com>
2023-03-06 20:20:07 +0300

    Add contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import Button from "$lib/Button.svelte";
+	import { ClipboardText, Placeholder, PlusCircle, ShoppingCart } from "phosphor-svelte";
+	import InputBox from "./InputBox.svelte";
+</script>
+
+<div class="content">
+  <div class="title-layout">
+    <div class="title-text-layout">
+      <h1>Contact</h1>
+      <p class="normal-text">If you have your idea ready for a project, get in touch with me right away, and I’ll give you an estimate of the cost in a moment’s notice!</p>
+      <p class="normal-text">You can alternatively email me at:</p>
+      <a class="normal-text email" href="mailto:m2.matt2e2@gmail.com">m2.matt2e2@gmail.com</a>
+    </div>
+    <div class="_2rem-layout">
+      <Button icon={ShoppingCart} link={"/order"} text={"pricings"} />
+      <Button icon={ClipboardText} link={"/terms"} text={"read the terms"} style={"outlined"} />
+    </div>
+  </div>
+</div>
+
+<div class="content">
+  <form id="contact-form" action="submit" class="grid">
+    <InputBox placeholder={"john.doe@gmail.com"} type={"email"} label={"Subject *"}/>
+    <InputBox placeholder={"Write a short, general overview of your project."} label={"Title *"}/>
+    <InputBox placeholder={"Give me a few details of what your project is about, what I should know etc."} multiline={true} label={"Project Details *"}/>
+  </form>
+  <div class="_2rem-layout">
+    <Button icon={PlusCircle} text={"Submit request"} type={"submit"} form={"contact-form"} link={"/lmao"} />
+  </div>
+</div>
+
+<style>
+  .email {
+    color: var(--accent-two);
+  }
+  .email:hover {
+    text-decoration: underline;
+  }
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 4rem;
+    row-gap: 2rem;
+  }
+  #contact-form {
+    margin-bottom: 2rem;
+  }
+  .content:nth-child(2) {
+    margin-top: 0;
+  }
+</style>
---
+++ src/routes/contact/InputBox.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  export let placeholder: string = "Placeholder text";
+  export let multiline: boolean = false;
+  export let type: string = "text";
+  export let label: string;
+</script>
+
+<div class="input-box">
+  <label for="subject" class="bold-text">{label}</label>
+  {#if !multiline}
+    <input {type} {placeholder} class="normal-text" required>
+  {:else}
+  <div class="wrap">
+    <img src="DotsSix.svg" alt="resize grabber"/>
+    <textarea {placeholder} class="normal-text" rows="5" required></textarea>
+  </div>
+  {/if}
+</div>
+
+<style>
+  .input-box {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+  input, textarea {
+    width: 100%;
+    padding: 1rem 1.5rem;
+    background: rgba(0, 0, 0, 0.32);
+    border: 4px solid rgba(245, 245, 245, 0.32);
+    border-radius: 16px;
+    color: white;
+    transition: border .2s ease-in-out;
+  }
+  input:invalid:not(:placeholder-shown) {
+    border: 4px solid var(--accent);
+  }
+  input:valid, textarea:valid {
+    border: 4px solid var(--accent-two);
+    transition: border .2s ease-in-out;
+  }
+  textarea {
+    resize: vertical;
+  }
+  .wrap {
+    position: relative;
+  }
+  img {
+    position: absolute;
+    right: 8px;
+    bottom: 4px;
+    pointer-events: none;
+    z-index: 1;
+  }
+  ::-webkit-resizer {
+    display: none;
+  }
+</style>
